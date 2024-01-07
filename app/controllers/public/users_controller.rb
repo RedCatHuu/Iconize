1a3602364c8123d95181d61b3ff71e66391728f7
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-01-07 09:31:14 +0000

    [add]user編集、マイページ画面即席

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,11 +1,20 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      redirect_to my_page_user_path(user)
+      flash[:notice] = "編集完了。フラッシュメッセージはいらないかも"
+    else
+      render :show
+    end
   end
 
   def confirm
@@ -19,4 +28,11 @@ class Public::UsersController < ApplicationController
 
   def followers
   end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end 
+  
 end
