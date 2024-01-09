130f513aba139d0f9b5d9566b1b4187e31d0f0f4
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-01-09 04:00:34 +0000

    [add]管理者report_show。その他雑多な修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_08_145446) do
+ActiveRecord::Schema.define(version: 2024_01_09_022224) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2024_01_08_145446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title", null: false
+    t.boolean "is_published", default: true, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
