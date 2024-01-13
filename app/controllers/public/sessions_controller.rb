6fcb09b147ab8ef6a5b4ec56004c029a51ee24f4
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-01-13 03:42:14 +0000

    [add]ゲストユーザー

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,13 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :user_state, only: [:create]
+  
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to works_path, notice: "ゲストユーザーでログインしました。"
+  end 
+  
   # GET /resource/sign_in
   # def new
   #   super
