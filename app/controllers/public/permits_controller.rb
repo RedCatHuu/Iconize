47dcfcb0edaa1c4534b9c2e5033e4a384ab66eeb
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-01-30 10:47:27 +0000

    [fix]微修正

--- app/controllers/public/permits_controller.rb
+++ app/controllers/public/permits_controller.rb
@@ -5,7 +5,7 @@ class Public::PermitsController < ApplicationController
     @club = Club.find(params[:club_id])
     permit = current_user.permits.new(club_id: @club.id)
     permit.save
-    redirect_to club_path(@club), notice: "参加申請を送りました。"
+    redirect_to club_path(@club), notice: "加入申請を送りました。"
     
   end
   
@@ -13,7 +13,7 @@ class Public::PermitsController < ApplicationController
     @club = Club.find(params[:club_id])
     permit = current_user.permits.find_by(club_id: @club.id)
     permit.destroy
-    redirect_to club_path(@club), notice: "参加申請を取り消しました。"
+    redirect_to club_path(@club), notice: "加入申請を取り消しました。"
   end
   
 end
