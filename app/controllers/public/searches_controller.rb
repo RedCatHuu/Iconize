b816623380a07cfe5ca717511ff55f009b0ec996
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-02-10 10:35:23 +0000

    [update]2以上単語での検索。userのみ導入

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -2,10 +2,24 @@ class Public::SearchesController < ApplicationController
   
   def search
     search_word = params[:word]
-    @users = User.where("name LIKE ?", "%#{search_word}%").active.page(params[:page]).per(24)
+    # @users = User.where("name LIKE ?", "%#{search_word}%").active.page(params[:page]).per(24)
+    @users = search_words(search_word, User, "name").active.page(params[:page]).per(24)
     @works = Work.where("title LIKE ?", "%#{search_word}%").public.page(params[:page]).per(24)
     @works_all = Work.where("title LIKE ?", "%#{search_word}%").public
     @clubs = Club.where("name LIKE ?", "%#{search_word}%").page(params[:page]).per(24)
+  end
+  
+  private
+  
+  def search_words(words, model, column)
+    keywords = words.split(/[[:blank:]]+/).select(&:present?)
+    # negative_keywords, positive_keywords = keywords.partition(&:start_with?("-"))
+    @model = model
+    # positive_keywords.each do |keyword|
+    keywords.each do |keyword|
+      @model = @model.where("#{column} LIKE ?", "%#{keyword}%")
+    end
+    return @model
   end
   
 end
