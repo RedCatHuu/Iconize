2a06bcec1def7e7a9a6f1d510e8551d1d3b90bc6
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2024-02-11 03:04:50 +0000

    [update]2単語以上検索及びマイナス検索実装

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,25 +1,29 @@
 class Public::SearchesController < ApplicationController
   
   def search
-    search_word = params[:word]
-    # @users = User.where("name LIKE ?", "%#{search_word}%").active.page(params[:page]).per(24)
-    @users = search_words(search_word, User, "name").active.page(params[:page]).per(24)
-    @works = Work.where("title LIKE ?", "%#{search_word}%").public.page(params[:page]).per(24)
-    @works_all = Work.where("title LIKE ?", "%#{search_word}%").public
-    @clubs = Club.where("name LIKE ?", "%#{search_word}%").page(params[:page]).per(24)
+    @words = params[:word]
+    @users = search_words(@words, User, "name").active.page(params[:page]).per(24)
+    @works = search_words(@words, Work, "title").public.page(params[:page]).per(24)
+    @works_all = search_words(@words, Work, "title").public
+    @clubs = search_words(@words, Club, "name").page(params[:page]).per(24)
   end
   
   private
   
   def search_words(words, model, column)
     keywords = words.split(/[[:blank:]]+/).select(&:present?)
-    # negative_keywords, positive_keywords = keywords.partition(&:start_with?("-"))
-    @model = model
-    # positive_keywords.each do |keyword|
-    keywords.each do |keyword|
-      @model = @model.where("#{column} LIKE ?", "%#{keyword}%")
+    negative_keywords, positive_keywords = keywords.partition { |keyword| keyword.start_with?("-") }
+    result = model
+    
+    positive_keywords.each do |keyword|
+      result = result.where("#{column} LIKE ?", "%#{keyword}%")
     end
-    return @model
+    
+    negative_keywords.each do |keyword|
+      result.where!("#{column} NOT LIKE ?", "%#{keyword.delete_prefix("-")}%")
+    end
+    
+    return result
   end
   
 end
